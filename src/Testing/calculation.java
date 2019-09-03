d900e89c32f63cb75a219e9e5fa453cf1680a812
Your Name <shraddha12salvi@gmail.com>
2019-09-03 16:37:55 +0530

    first commit

---
+++ src/Testing/calculation.java
@@ -0,0 +1,20 @@
+package Testing;
+
+public class calculation {
+   public int sum(int a, int b) {
+	return a+b;
+	   
+   }
+   public int sub(int a, int b) {
+	   return a-b;
+   }
+   public int mul(int a,int b) {
+	   return a*b;
+   }
+   
+   public int div(int a,int b) {
+	return a/b;
+	   
+   }
+}
+
